eaa05938f2a996e796101c9960ef7e76148cc075
penixab13 <115022449+penixab13@users.noreply.github.com>
2024-05-22 12:58:49 +0100

    new_config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
   cognito: {
-    userPoolId: "us-west-2_03cfdmYmJ",
-    userPoolClientId: "67lp41kf4bluc3mullou8a91u1",
+    userPoolId: "us-west-2_gxXgs3kzR",
+    userPoolClientId: "2h92njuah6bkg3o6vqokglifm2",
     region: "us-west-2"
   },
   api: {
